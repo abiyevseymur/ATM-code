8944619ac1728e4370338938ca09b7ee494ffcd2
Prisoner1992 <abiyevseymur92@gmail.com>
2017-05-05 04:46:33 +0400

    ATM code C#

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Meblegi daxil edin:");
+            int pul = Convert.ToUInt16(Console.ReadLine());
+            int[] valute = new int[]{ 100, 50, 20, 10, 5, 1 };
+            for (int i= 0;i<6;i++)
+            {
+                int eded;
+                if (pul / valute[i] > 0) {
+                    eded = pul / valute[i];
+                    Console.WriteLine(valute[i]+" AZN "+ eded+ " eded");
+                    pul = pul % valute[i];
+                }
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+ATM Code on C# (compact)
